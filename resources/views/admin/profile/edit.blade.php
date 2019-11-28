2f3637c99251b88cad977e3098977bff7e86eb37
EC2 Default User <ec2-user@ip-172-31-35-138.us-east-2.compute.internal>
2019-11-28 14:20:46 +0000

    16 投稿したニュースを更新/削除

--- resources/views/admin/profile/edit.blade.php
+++ resources/views/admin/profile/edit.blade.php
@@ -1,12 +1,12 @@
 @extends('layouts.admin')
-@section('title', 'ニュースの新規作成')
+@section('title', 'プロフィールの編集')
 
 @section('content')
     <div class="container">
         <div class="row">
             <div class="col-md-8 mx-auto">
-                <h2>ニュース新規作成</h2>
-                <form action="{{ action('Admin\ProfileController@create') }}" method="post" enctype="multipart/form-data">
+                <h2>プロフィール編集</h2>
+                <form action="{{ action('Admin\ProfileController@update') }}" method="post" enctype="multipart/form-data">
 
                     @if (count($errors) > 0)
                         <ul>
@@ -16,37 +16,34 @@
                         </ul>
                     @endif
                     <div class="form-group row">
-                        <label class="col-md-2">氏名</label>
+                        <label class="col-md-2" for="title">氏名</label>
                         <div class="col-md-10">
-                            <input type="text" class="form-control" name="name" value="{{ old('title') }}">
-                        </div>
-                    </div>
+                            <input type="text" class="form-control" name="name" value="{{ $profile_form->title }}">
+                            </div></div>
+                        <div class="form-group row">
+                        <label class="col-md-2" for="title">性別</label>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" name="gender" value="{{ $profile_form->title }}">
+                        </div></div>    
+                        <div class="form-group row">
+                        <label class="col-md-2" for="title">趣味</label>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" name="hobby" value="{{ $profile_form->title }}">
+                        </div></div>
                     <div class="form-group row">
-                        <label class="col-md-2">性別</label>
+                        <label class="col-md-2" for="body">自己紹介</label>
                         <div class="col-md-10">
-                            <input type="text" class="form-control" name="gender" value="{{ old('title') }}">
+                            <textarea class="form-control" name="introduction" rows="20">{{ $profile_form->body }}</textarea>
                         </div>
                     </div>
+                    
                     <div class="form-group row">
-                        <label class="col-md-2">趣味</label>
                         <div class="col-md-10">
-                            <input type="text" class="form-control" name="hobby" value="{{ old('title') }}">
+                            <input type="hidden" name="id" value="{{ $profile_form->id }}">
+                            {{ csrf_field() }}
+                            <input type="submit" class="btn btn-primary" value="更新">
                         </div>
                     </div>
-                    <div class="form-group row">
-                        <label class="col-md-2">自己紹介欄</label>
-                        <div class="col-md-10">
-                            <textarea class="form-control" name="introduction" rows="20">{{ old('body') }}</textarea>
-                        </div>
-                    </div>
-                    <div class="form-group row">
-                        <label class="col-md-2">画像</label>
-                        <div class="col-md-10">
-                            <input type="file" class="form-control-file" name="image">
-                        </div>
-                    </div>
-                    {{ csrf_field() }}
-                    <input type="submit" class="btn btn-primary" value="更新">
                 </form>
             </div>
         </div>
