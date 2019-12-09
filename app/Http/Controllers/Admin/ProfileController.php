fec490ca32b0422e33b50c1099bd209b880393c0
EC2 Default User <ec2-user@ip-172-31-35-138.us-east-2.compute.internal>
2019-12-09 13:25:40 +0000

    17 編集履歴を実装

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Profile;
+use App\Profilehistory;
+use Carbon\Carbon;
+
 class ProfileController extends Controller
 {
   public function add()
@@ -16,7 +19,7 @@ class ProfileController extends Controller
   {
       // Varidationを行う
       $this->validate($request, Profile::$rules);
-      $profile = new ProfileControllerrofile;
+      $profile = new Profile;
       $form = $request->all();
 
       // formに画像があれば、保存する
@@ -44,7 +47,7 @@ class ProfileController extends Controller
           // 検索されたら検索結果を取得する
           $posts = Profile::where('title', $cond_title)->get();
       } else {
-          // それ以外はすべてのニュースを取得する
+          // それ以外はすべてのプロフィールを取得する
           $posts = Profile::all();
       }
       return view('admin.profile.index', ['posts' => $posts, 'cond_title' => $cond_title]);
@@ -54,7 +57,7 @@ class ProfileController extends Controller
   public function edit(Request $request)
   {
       // Profile Modelからデータを取得する
-      $profile = Profile::find($request->max('date'));
+      $profile = Profile::find($request->id);
       if (empty($profile)) {
         abort(404);    
       }
@@ -63,27 +66,32 @@ class ProfileController extends Controller
 
 
   public function update(Request $request)
-  {
-      // Validationをかける
-      $this->validate($request, Profile::$rules);
-      // Profile Modelからデータを取得する
-      $profile = Profile::find($request->id);
-      // 送信されてきたフォームデータを格納する
-      $profile_form = $request->all();
-      if (isset($profile_form['image'])) {
-        $path = $request->file('image')->store('public/image');
-        $profile->image_path = basename($path);
-        unset($profile_form['image']);
-      } elseif (isset($request->remove)) {
-        $profile->image_path = null;
-        unset($profile_form['remove']);
-      }
-      unset($profile_form['_token']);
-      // 該当するデータを上書きして保存する
-      $profile->fill($profile_form)->save();
+    {
+        $this->validate($request, Profile::$rules);
+        $profile = Profile::find($request->id);
+        $profile_form = $request->all();
+        if ($request->remove == 'true') {
+            $profile_form['image_path'] = null;
+        } elseif ($request->file('image')) {
+            $path = $request->file('image')->store('public/image');
+            $profile_form['image_path'] = basename($path);
+        } else {
+            $profile_form['image_path'] = $profile->image_path;
+        }
 
-      return redirect('admin/profile');
-  }
+        unset($profile_form['_token']);
+        unset($profile_form['image']);
+        unset($profile_form['remove']);
+        $profile->fill($profile_form)->save();
+
+        // 以下を追記　編集履歴の記録と参照
+        $history = new Profilehistory;
+        $history->profile_id = $profile->id;
+        $history->edited_at = Carbon::now();
+        $history->save();
+
+        return redirect('admin/profile/');
+    }
   public function delete(Request $request)
   {
       // 該当するProfile Modelを取得
